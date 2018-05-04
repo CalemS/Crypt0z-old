613846e7089069ad35aef4242e4c983096b37b68
Calem John Smith <calem_smith@zoho.com>
2018-05-04 18:48:41 +0930

    Update bitrpc.py

--- contrib/bitrpc/bitrpc.py
+++ contrib/bitrpc/bitrpc.py
@@ -10,9 +10,9 @@ rpcpass = ""
 
 
 if rpcpass == "":
-	access = ServiceProxy("http://127.0.0.1:2600")
+	access = ServiceProxy("http://127.0.0.1:14379")
 else:
-	access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:2600")
+	access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:14379")
 cmd = sys.argv[1].lower()
 
 if cmd == "backupwallet":
