ddbe3d6871331d39f6a0bb6c6e747220a007f4f1
Calem John Smith <calem_smith@zoho.com>
2018-05-06 16:19:20 +0930

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1173,7 +1173,8 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {NULL, NULL}
+{www.crypr0z.com, 18.188.133.48:rpc}
+{NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
