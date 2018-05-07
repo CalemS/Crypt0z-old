5cfed6adcffff141120e31b149a96e39e3c6c4b8
Calem John Smith <calem_smith@zoho.com>
2018-05-07 18:30:21 +0930

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1172,13 +1172,11 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"ec2-18-188-133-48.us-east-2.compute.amazonaws.com"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"ec2-18-188-133-48.us-east-2.compute.amazonaws.com",
-    {NULL, NULL}
+    NULL, NULL
 };
  
 void ThreadDNSAddressSeed()
